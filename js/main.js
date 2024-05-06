05942a4977d6e62fc5846dd0e00fe79a35762a48
francesco madonna <fra.mad.93@hotmail.it>
2024-05-06 17:47:43 +0200

    prima commit con array

---
+++ js/main.js
@@ -0,0 +1,49 @@
+const staff = [
+
+    {
+        Nome: "Wayne"
+        Cognome: "Barnett"
+        Ruolo: "Founder & CEO"
+        Image: "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {
+        Nome: "Angela"
+        Cognome: "Caroll"
+        Ruolo: "Chief Editor"
+        Image: "angela-caroll-chief-editor.jpg"
+
+    }
+
+    {
+        Nome: "Walter"
+        Cognome: "Gordon"
+        Ruolo: "Office Manager"
+        Image: "walter-gordon-office-manager.jpg"
+
+    }
+
+    {
+        Nome: "Angela"
+        Cognome: "Lopez"
+        Ruolo: "Social Media Manager"
+        Image: "angela-lopez-social-media-manager.jpg"
+
+    }
+    {
+        Nome: "Scott"
+        Cognome: "Estrada"
+        Ruolo: "Developer"
+        Image: "scott-estrada-developer.jpg"
+
+    }
+    {
+        Nome: "Barbara"
+        Cognome: "Ramos"
+        Ruolo: "Graphic Designer"
+        Image: "barbara-ramos-graphic-designer.jpg"
+
+    }
+]
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+1creiamo un arrey di oggetti con i dati fornitici;
+2provare a stampare su console il nome il ruolo del soggett;
+3stampare in pagina nome tuolo e imaggini come strighe;
