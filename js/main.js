ae0ad69aea98eb76e4256e3dcaa8c13379083246
francesco madonna <fra.mad.93@hotmail.it>
2024-05-07 10:22:34 +0200

    finito

--- js/main.js
+++ js/main.js
@@ -1,49 +1,63 @@
 const staff = [
 
     {
-        Nome: "Wayne"
-        Cognome: "Barnett"
-        Ruolo: "Founder & CEO"
-        Image: "wayne-barnett-founder-ceo.jpg"
+        Nome: "Wayne",
+        Cognome: "Barnett",
+        Ruolo: "Founder & CEO",
+        Image: "wayne-barnett-founder-ceo.jpg",
     },
 
     {
-        Nome: "Angela"
-        Cognome: "Caroll"
-        Ruolo: "Chief Editor"
-        Image: "angela-caroll-chief-editor.jpg"
+        Nome: "Angela",
+        Cognome: "Caroll",
+        Ruolo: "Chief Editor",
+        Image: "angela-caroll-chief-editor.jpg",
 
-    }
-
-    {
-        Nome: "Walter"
-        Cognome: "Gordon"
-        Ruolo: "Office Manager"
-        Image: "walter-gordon-office-manager.jpg"
-
-    }
+    },
 
     {
-        Nome: "Angela"
-        Cognome: "Lopez"
-        Ruolo: "Social Media Manager"
-        Image: "angela-lopez-social-media-manager.jpg"
+        Nome: "Walter",
+        Cognome: "Gordon",
+        Ruolo: "Office Manager",
+        Image: "walter-gordon-office-manager.jpg",
 
-    }
+    },
+
     {
-        Nome: "Scott"
-        Cognome: "Estrada"
-        Ruolo: "Developer"
-        Image: "scott-estrada-developer.jpg"
+        Nome: "Angela",
+        Cognome: "Lopez",
+        Ruolo: "Social Media Manager",
+        Image: "angela-lopez-social-media-manager.jpg",
 
-    }
+    },
     {
-        Nome: "Barbara"
-        Cognome: "Ramos"
-        Ruolo: "Graphic Designer"
-        Image: "barbara-ramos-graphic-designer.jpg"
+        Nome: "Scott",
+        Cognome: "Estrada",
+        Ruolo: "Developer",
+        Image: "scott-estrada-developer.jpg",
 
-    }
+    },
+    {
+        Nome: "Barbara",
+        Cognome: "Ramos",
+        Ruolo: "Graphic Designer",
+        Image: "barbara-ramos-graphic-designer.jpg",
+
+    },
 ]
+console.log(staff)
 
 
+const cardMember = document.getElementById("card");
+console.log(cardMember);
+
+for (let i = 0; i < staff.length; i++) {
+    const member = staff[i];
+    console.log(member);
+
+    cardMember.innerHTML += `
+    <h1>${member["Nome"]}</h1>
+    <h3>${member["Ruolo"]}</h3>
+    <img>${member["Image"]}
+  `
+}
